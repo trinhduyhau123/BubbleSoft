beaff4aac1dd73dfc6359b42bbc70c3c0efc09d2
Trinh Hau <congtuqwe@gmail.com>
2018-09-20 10:22:31 +0700

    BubbleSoft

---
+++ src/BubbleSoft.java
@@ -0,0 +1,25 @@
+public class BubbleSoft {
+    static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
+
+    public static void bubblesoft(int[] list) {
+        boolean needNextPass = true;
+        for (int k = 1; k < list.length && needNextPass; k++) {
+            needNextPass = false;
+            for (int i = 0; i < list.length - k; i++) {
+                if (list[i] > list[i + 1]) {
+                    int temp = list[i];
+                    list[i] = list[i + 1];
+                    list[i + 1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        bubblesoft(list);
+        for (int i = 0; i < list.length; i++) {
+            System.out.printf(list[i]+ "\t");
+        }
+    }
+}
